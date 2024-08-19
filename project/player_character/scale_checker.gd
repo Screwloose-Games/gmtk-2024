ba7b200a8d61735e409a2927ceb1aa08135b6c3e
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2024-08-19 16:02:46 -0700

    fix scale checker

--- project/player_character/scale_checker.gd
+++ project/player_character/scale_checker.gd
@@ -6,5 +6,5 @@ func _physics_process(delta: float) -> void:
 	#var trajectory = move_toward(global_position, player.global_position, delta)
 	var direction = (player.global_position - global_position).normalized()
 	velocity = direction * speed
-	#move_and_slide()
-	move_and_collide()
+	move_and_slide()
+	#move_and_collide()
