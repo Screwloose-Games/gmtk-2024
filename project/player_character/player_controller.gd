7c5447fa14a72dc7b32f847d5b041e6f2e227dd6
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2024-08-19 15:11:02 -0700

    Add coyote time and float

--- project/player_character/player_controller.gd
+++ project/player_character/player_controller.gd
@@ -2,6 +2,9 @@
 class_name Actor
 extends CharacterBody2D
 
+var _is_coyote_time : bool = false
+@export var coyote_time_amount: float = 0.2
+
 @onready var character_scaler: Node = %CharacterScaler
 
 @onready var state_machine: StateMachine = %StateMachine
@@ -21,13 +24,45 @@ extends CharacterBody2D
 
 @onready var base_jump_velocity = -jump_power
 @onready var jump_velocity = -jump_power
+@export var max_fall_speed: float = 250.0
+@onready var max_float_fall_speed: float:
+	get: return max_fall_speed * scale_amount
 
 signal scaled(amount: float)
 signal jumped
 signal became_airborn
 signal landed
 
+var _is_on_floor: bool = true
+
+func _check_floor():
+	if _is_on_floor and not is_on_floor():
+		_on_leave_floor()
+		
+	if not _is_on_floor and is_on_floor():
+		_on_touch_floor()
+	
+	_is_on_floor = is_on_floor()
+
+func _on_touch_floor():
+	stop_coyote_time()
+
+func stop_coyote_time():
+	_is_coyote_time = false
+	_is_on_floor = false
+
+func _on_leave_floor():
+	if velocity.y >= 0.0:
+		start_coyote_time()
+
 @export var base_push_force: float = 100.0
+
+func start_coyote_time():
+	if _is_coyote_time:
+		return
+	_is_coyote_time = true
+	await get_tree().create_timer(coyote_time_amount).timeout
+	_is_coyote_time = false
 
 var push_force: float:
 	get: return base_push_force * scale_amount
@@ -50,21 +85,47 @@ func calculate_jump_velocity(gravity_strength: float, jump_height: float) -> flo
 func _on_character_scaled(amount: float):
 	scaled.emit(amount)
 
+func _apply_jump():
+	#if not _can_move: return
+	
+	if Input.is_action_just_pressed("jump"):
+		if _can_jump():
+			velocity.y = jump_velocity
+			state_machine.change_state(%JumpState)
+			jumped.emit()
+			#on_jump_start.emit()
+			stop_coyote_time()
+		#else:
+			#_start_jump_buffer()
+
+	#elif Input.is_action_just_released("jump"):
+		#on_jump_end.emit()
+
+func _can_jump():
+	return _is_coyote_time or _is_on_floor
+
 func _ready():
 	character_scaler.scaled.connect(_on_character_scaled)
 
+
 func _physics_process(delta: float) -> void:
 	var grav = get_gravity()
+	_check_floor()
+	
 	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += character_gravity * delta
+		if velocity.y > 0 and Input.is_action_pressed("jump"):
+			velocity.y = min(max_float_fall_speed, velocity.y)
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
-		state_machine.change_state(%JumpState)
-		jumped.emit()
+	_apply_jump()
+	#if Input.is_action_just_pressed("jump") and (is_on_floor() or _is_coyote_time):
+		#_is_coyote_time = false
+		#velocity.y = jump_velocity
+		#state_machine.change_state(%JumpState)
+		#jumped.emit()
 		
 
 	# Get the input direction and handle the movement/deceleration.
