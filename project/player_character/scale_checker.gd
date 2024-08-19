ae376d08c18b3632dc481ebca7d337d068384667
Jonathan David Lewis <jonathandavidlewis@gmail.com>
2024-08-19 16:01:09 -0700

    add scale check

---
+++ project/player_character/scale_checker.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+
+@export var player: CharacterBody2D
+@export var speed: float = 200.0
+func _physics_process(delta: float) -> void:
+	#var trajectory = move_toward(global_position, player.global_position, delta)
+	var direction = (player.global_position - global_position).normalized()
+	velocity = direction * speed
+	#move_and_slide()
+	move_and_collide()
